d2be73ddd6eadeb6014a00a1e52ff359abea6a86
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-02-13 12:40:02 -0800

    created search and category section

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,23 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useProducts } from "../context/ProductContext";
-
-import styles from "./ProductsPage.module.css";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 
+import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
+
+import styles from "./ProductsPage.module.css";
+
 function ProductsPage(props) {
   const products = useProducts();
-  console.log(products);
+
+  const [displayed, setDisplayed] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
+
+  useEffect(() => {
+    setDisplayed(products);
+  }, [products]);
+
+  useEffect(() => {
+    console.log(query);
+  }, [query]);
+
+  const searchHandler = () => {
+    setQuery((query) => ({ ...query, search }));
+  };
+
+  const categoryHandler = (e) => {
+    const { tagName } = e.target;
+    console.log(e.target.innerText);
+    const category = e.target.innerText.toLowerCase();
+
+    console.log(tagName);
+    if (tagName !== "LI") return;
+    setQuery((query) => ({ ...query, category }));
+  };
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((product) => (
-          <Card key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch></ImSearch>
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {displayed.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelery</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
