84a279c274fc99c0446e7f830bbc86a14720e1a2
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-02-13 11:57:52 -0800

    created and styles Card component

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { useProducts } from "../context/ProductContext";
 
+import styles from "./ProductsPage.module.css";
+import Card from "../components/Card";
+
 function ProductsPage(props) {
   const products = useProducts();
   console.log(products);
-  return <div>product page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>Loading ...</p>}
+        {products.map((product) => (
+          <Card key={product.id} data={product} />
+        ))}
+      </div>
+      <div>sidebar</div>
+    </div>
+  );
 }
 
 export default ProductsPage;
