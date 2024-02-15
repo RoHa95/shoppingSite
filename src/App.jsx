3439bad1302dfba7381846c6faf92b9fe6938af0
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-02-14 22:28:30 -0800

    create layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./layout/Layout";
 import ProductsPage from "./pages/ProductsPage";
 import DetailsPage from "./pages/DetailsPage";
 import CheckoutPage from "./pages/CheckoutPage";
@@ -9,15 +10,17 @@ function App() {
   return (
     <div>
       <CartProvider>
-        <ProductsProvider>
-          <Routes>
-            <Route path="/" element={<Navigate to="/products" replace />} />
-            <Route path="/products" element={<ProductsPage />} />
-            <Route path="/products/:id" element={<DetailsPage />} />
-            <Route path="/checkout" element={<CheckoutPage />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </ProductsProvider>
+        <Layout>
+          <ProductsProvider>
+            <Routes>
+              <Route path="/" element={<Navigate to="/products" replace />} />
+              <Route path="/products" element={<ProductsPage />} />
+              <Route path="/products/:id" element={<DetailsPage />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </ProductsProvider>
+        </Layout>
       </CartProvider>
     </div>
   );
