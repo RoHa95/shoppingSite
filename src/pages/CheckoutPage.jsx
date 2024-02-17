3ae370d7acdd52e35435a2b185a8b466d6ff9836
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-02-16 22:12:45 -0800

    created basket card component

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { useCart } from "../context/CartContext";
+import BasketCard from "../components/BasketCard";
 function CheckoutPage(props) {
-  return <div>check out page</div>;
+  const [state, dispatch] = useCart();
+  const clickHandler = (type, payload) => {
+    dispatch({ type, payload });
+  };
+  return (
+    <div>
+      <div>
+        {state.selectedItems.map((p) => (
+          <BasketCard key={p.id} data={p} clickHandler={clickHandler} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CheckoutPage;
