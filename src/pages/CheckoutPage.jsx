15a1d335afa4f4d223c08811debcf1a7c6dd3c58
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-02-16 22:44:55 -0800

    create basket sidebar component

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { useCart } from "../context/CartContext";
 import BasketCard from "../components/BasketCard";
+import BasketSidebar from "../components/BasketSidebar";
+
+import styles from "./CheckoutPage.module.css";
+
 function CheckoutPage(props) {
   const [state, dispatch] = useCart();
   const clickHandler = (type, payload) => {
     dispatch({ type, payload });
   };
+  if (!state.itemsCounter) {
+    return (
+      <div className={styles.container}>
+        <p style={{ margin: "10px" }}>Empety!</p>
+      </div>
+    );
+  }
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <BasketSidebar state={state} clickHandler={clickHandler} />
+      <div className={styles.products}>
         {state.selectedItems.map((p) => (
           <BasketCard key={p.id} data={p} clickHandler={clickHandler} />
         ))}
